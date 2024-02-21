ef113ff6d4f48f9bc2c3c148f0e1e1fbb9745255
SuhrobDev <suhrobqayumov4@gmail.com>
2024-02-21 13:52:41 +0500

    fix 3

--- app/src/main/java/com/example/movieapp/fragment/HomeFragment.kt
+++ app/src/main/java/com/example/movieapp/fragment/HomeFragment.kt
@@ -4,10 +4,15 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import androidx.fragment.app.Fragment
+import com.example.movieapp.MymodelUI
+import com.example.movieapp.R
 import com.example.movieapp.databinding.FragmentHomeBinding
+import com.example.movieapp.itemAdapter
 
 class HomeFragment : Fragment() {
+    private lateinit var adapter: itemAdapter
 
     private lateinit var binding: FragmentHomeBinding
     override fun onCreateView(
@@ -15,7 +20,24 @@ class HomeFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View {
         binding = FragmentHomeBinding.inflate(layoutInflater)
+        adapter = itemAdapter {
+            Toast.makeText(requireContext(), "clicked", Toast.LENGTH_SHORT).show()
+        }
+        //hello
+        binding.horizontalRv.adapter = adapter
 
+
+        val toplist = listOf<MymodelUI>(
+            MymodelUI(R.drawable.shangchi, "SHang-Chi", id = 1),
+            MymodelUI(R.drawable.shangchi, "Eternals", id = 2),
+            MymodelUI(R.drawable.shangchi, "Avengers", id = 3),
+            MymodelUI(R.drawable.shangchi, "The New Amazing Spider-Man", id = 4),
+            MymodelUI(R.drawable.shangchi, "Electro", id = 5),
+            MymodelUI(R.drawable.shangchi, "Sherlok Holmas", id = 6),
+            MymodelUI(R.drawable.shangchi, "Elona Holmas", id = 7),
+        )
+
+        adapter.setList(toplist)
         return binding.root
     }
 
