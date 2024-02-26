8b84c0eb9835ab430928fb0c897c109e4a827170
SuhrobDev <suhrobqayumov4@gmail.com>
2024-02-26 13:54:21 +0500

    bottom navigation set up

--- app/src/main/java/com/example/movieapp/MainActivity2.kt
+++ app/src/main/java/com/example/movieapp/MainActivity2.kt
@@ -1,10 +1,12 @@
 package com.example.movieapp
 
 import android.os.Bundle
+import android.view.View
 import androidx.appcompat.app.AppCompatActivity
 import androidx.navigation.NavController
 import androidx.navigation.fragment.NavHostFragment
 import androidx.navigation.fragment.findNavController
+import androidx.navigation.ui.setupWithNavController
 import com.example.movieapp.databinding.ActivityMain2Binding
 
 class MainActivity2 : AppCompatActivity() {
@@ -20,5 +22,18 @@ class MainActivity2 : AppCompatActivity() {
             supportFragmentManager.findFragmentById(R.id.my_fragment_container) as NavHostFragment
         navController = navHostFragment.findNavController()
 
+        navController.addOnDestinationChangedListener { _, destination, _ ->
+
+            if (destination.id == R.id.detailFragment) {
+                //hide
+                binding.bottomNav.visibility=View.GONE
+            }else{
+            //show
+                binding.bottomNav.visibility=View.VISIBLE
+            }
+
+        }
+
+        binding.bottomNav.setupWithNavController(navController)
     }
 }
