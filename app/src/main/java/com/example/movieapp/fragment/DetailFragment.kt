ed34e03ca4454864b25d25712778353831af12fb
MuhammadmirzoNacibkhanov <Mirjalolnacibkhanov81@gmail.com>
2024-02-27 21:03:53 +0500

    changed

--- app/src/main/java/com/example/movieapp/fragment/DetailFragment.kt
+++ app/src/main/java/com/example/movieapp/fragment/DetailFragment.kt
@@ -5,24 +5,23 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
-import com.example.movieapp.databinding.FragmentDetailBinding
+//import com.example.movieapp.databinding.DetailFragment
+
 
 class DetailFragment : Fragment() {
+//
+//    private lateinit var binding: DetailFragment
+//
+//    override fun onCreateView(
+//        inflater: LayoutInflater, container: ViewGroup?,
+//        savedInstanceState: Bundle?
+//    ): View {
+//        binding = FragmentDetailBinding.inflate(layoutInflater)
+//        return binding.root
+//    }
+//
 
-    private lateinit var binding: FragmentDetailBinding
-    override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View {
-        binding = FragmentDetailBinding.inflate(layoutInflater)
 
-
-
-
-
-
-        return binding.root
-    }
 
 
 }
