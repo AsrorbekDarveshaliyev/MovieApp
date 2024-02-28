f7c30fe95b126f077e0b5a0d65891991b6d05bec
SuhrobDev <suhrobqayumov4@gmail.com>
2024-02-28 14:31:53 +0500

    passing data between fragments

--- app/src/main/java/com/example/movieapp/fragment/DetailFragment.kt
+++ app/src/main/java/com/example/movieapp/fragment/DetailFragment.kt
@@ -10,15 +10,32 @@ import com.example.movieapp.databinding.FragmentDetailBinding
 class DetailFragment : Fragment() {
 
     private lateinit var binding: FragmentDetailBinding
+    private var img = 0
+    private lateinit var name: String
+    private lateinit var rating: String
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
         binding = FragmentDetailBinding.inflate(layoutInflater)
 
+        binding.name.text=name
 
         return binding.root
     }
 
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        //receive passed argument from bundle
+
+        arguments?.let {
+
+            name = it.getString("name", "")
+            img = it.getInt("img")
+            rating = it.getString("rating", "")
+        }
+
+    }
 
 }
--- app/src/main/java/com/example/movieapp/fragment/HomeFragment.kt
+++ app/src/main/java/com/example/movieapp/fragment/HomeFragment.kt
@@ -4,7 +4,6 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.Toast
 import androidx.fragment.app.Fragment
 import androidx.navigation.fragment.findNavController
 import com.example.movieapp.MymodelUI
@@ -16,6 +15,7 @@ class HomeFragment : Fragment() {
     private lateinit var adapter: itemAdapter
 
     private lateinit var binding: FragmentHomeBinding
+    private lateinit var bundle: Bundle
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
@@ -23,8 +23,12 @@ class HomeFragment : Fragment() {
         binding = FragmentHomeBinding.inflate(layoutInflater)
 
 
-        adapter = itemAdapter {
-            findNavController().navigate(R.id.detailFragment)
+        bundle = Bundle()
+        adapter = itemAdapter { data ->
+            bundle.putString("name", data.name)
+            bundle.putString("rating", data.rating)
+            bundle.putInt("img", data.image)
+            findNavController().navigate(R.id.detailFragment, bundle)
         }
         //hello
         binding.horizontalRv.adapter = adapter
@@ -39,7 +43,12 @@ class HomeFragment : Fragment() {
             MymodelUI(R.drawable.movie_venom, "SHang-Chi", id = 1, rating = "9.8/10 IMDb"),
             MymodelUI(R.drawable.movie_venom, "Eternals", id = 2, rating = "5.2/10 IMDb"),
             MymodelUI(R.drawable.movie_venom, "Avengers", id = 3, rating = "6.8/10 IMDb"),
-            MymodelUI(R.drawable.movie_venom, "The New Amazing Spider-Man", id = 4, rating = "7.8/10 IMDb"),
+            MymodelUI(
+                R.drawable.movie_venom,
+                "The New Amazing Spider-Man",
+                id = 4,
+                rating = "7.8/10 IMDb"
+            ),
             MymodelUI(R.drawable.movie_venom, "Electro", id = 5, rating = "7.8/10 IMDb"),
             MymodelUI(R.drawable.movie_venom, "Sherlok Holmas", id = 6, rating = "7.8/10 IMDb"),
             MymodelUI(R.drawable.movie_venom, "Elona Holmas", id = 7, rating = "7.8/10 IMDb"),
